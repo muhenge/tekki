6ee75c5576aca7e8dcf8aac5cdca0d7a335e906c
herve <mugunga5@gmail.com>
2022-03-04 00:05:27 +0200

    condition in login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,14 +2,14 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { 
-      success: true,
-      user: resource,
-      message: 'You are logged in.' ,
-      token: current_token,
-      }, status: :ok
+  
+    if resource.id != nil
+      render json: { success: true, user: resource,token: current_token, response: 'Authentication successfully' }, status: 201
+    else
+      render json: { success: false, response: 'Invalid credentials! Verify again' }, status: 401
+    end
   end
-
+  
   def respond_to_on_destroy
     render json: { message: 'logged out successfully!', }
   end
