955df5ec5f949ec8b28147d6c65da890f7cb0519
herve <mugunga5@gmail.com>
2022-03-13 12:18:15 +0200

    Correct session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,27 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
+
   private
+
   def respond_with(resource, _opts = {})
-    render json: {user:resource, token: current_token, message: 'Logged in successifully.' }, status: :ok
+    user = User.find_by_email(sign_in_params[:email])
+    render json: {user: user, token: current_token, message: 'Logged in successifully.' }, status: :ok
   end
 
-  # def log_out_success
-  #   render json: { message: "Logged out." }, status: :ok
-  # end
-  # def log_out_failure
-  #   render json: { message: "Logged out failure."}, status: :unauthorized
-  # end
+  def respond_to_on_failure(resource)
+    render json: {error: resource.errors.full_messages.join(', ')}, status: :unprocessable_entity
+  end
 
   def respond_to_on_destroy
-    # current_user ? log_out_success : log_out_failure
-    head :no_content
+    logout_success && return if current_user
+
+    logout_failure
+  end
+
+  def logout_success
+    render json: { message:"Logged out!" }, status: :ok
+  end
+  def logout_failure
+    render json: { message:"Failed to logout!" }, status: :unauthorized
   end
 end
