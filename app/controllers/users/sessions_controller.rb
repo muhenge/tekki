8e0dda958286c3830f53813b623d1e054e2cd181
herve <mugunga5@gmail.com>
2022-03-13 12:57:16 +0200

    Add condition session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,12 +5,17 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     user = User.find_by_email(sign_in_params[:email])
-    render json: {user: user, token: current_token, message: 'Logged in successifully.' }, status: :ok
+    if user && user.valid_password?(sign_in_params[:password])
+      render json: {user: user, token: current_token, message: 'Logged in successifully.' }, status: :ok
+    else
+      render json: {error: 'Invalid email or password.'}, status: :unauthorized
+    end
+    
   end
 
-  def respond_to_on_failure(resource)
-    render json: {error: resource.errors.full_messages.join(', ')}, status: :unprocessable_entity
-  end
+  # def respond_to_on_failure(resource)
+  #   render json: {error: resource.errors.full_messages.join(', ')}, status: :unprocessable_entity
+  # end
 
   def respond_to_on_destroy
     logout_success && return if current_user
