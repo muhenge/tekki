c98a0205abdb989ad0edb9ffbabb7adf46ee08b6
herve <mugunga5@gmail.com>
2022-04-25 16:26:39 +0200

    edit in progress

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,4 +9,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render json: {success: false, response: @user.errors.full_messages }, status: 401
     end
   end
+  def update
+    if @user.update(sign_up_params)
+      render json: {success: true, user:@user, response: "Updated successfully" }, status: 201
+    else
+      render json: {success: false, response: @user.errors.full_messages }, status: 401
+    end
+  end
 end
