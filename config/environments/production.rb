3f9d7d86831cba456b28d9a027dd43e6d7b4e0bb
muhenge <ngenziherve1@gmail.com>
2026-05-12 12:27:02 +0200

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   # config.cache_classes = true
-  config.hosts << "tekki-f7jp.onrender.com"
+  #config.hosts << "tekki-f7jp.onrender.com"
+  config.hosts << "https://teki-api.fly.dev/"
+  #config.hosts << "tekki.app"
   # config.hosts << /.*\.onrender\.com/
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
